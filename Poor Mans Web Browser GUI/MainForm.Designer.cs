7035831eb2e760735396c90cfcf10375ad5f9336
Silamoth <michaelbiancardi@icloud.com>
2019-01-27 16:05:19 -0600

    Add files via upload

---
+++ Poor Mans Web Browser GUI/MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Poor_Mans_Web_Browser_GUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourceTextBox = new System.Windows.Forms.TextBox();
+            this.serverTextBox = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sourceTextBox
+            // 
+            this.sourceTextBox.Location = new System.Drawing.Point(12, 12);
+            this.sourceTextBox.Multiline = true;
+            this.sourceTextBox.Name = "sourceTextBox";
+            this.sourceTextBox.ReadOnly = true;
+            this.sourceTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.sourceTextBox.Size = new System.Drawing.Size(898, 477);
+            this.sourceTextBox.TabIndex = 2;
+            // 
+            // serverTextBox
+            // 
+            this.serverTextBox.Location = new System.Drawing.Point(189, 495);
+            this.serverTextBox.Name = "serverTextBox";
+            this.serverTextBox.Size = new System.Drawing.Size(363, 20);
+            this.serverTextBox.TabIndex = 0;
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(790, 495);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 1;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 500);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Please enter a server to view:";
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.goButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 522);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.serverTextBox);
+            this.Controls.Add(this.sourceTextBox);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Poor Man\'s Web Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sourceTextBox;
+        private System.Windows.Forms.TextBox serverTextBox;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Poor Mans Web Browser GUI/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Poor_Mans_Web_Browser_GUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            sourceTextBox.Clear();
+
+            String server = serverTextBox.Text;
+
+            TcpClient client;
+
+            try
+            {
+                client = new TcpClient(server, 80);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK);
+                return;
+            }
+
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+
+            try
+            {
+                writer.WriteLine("GET / HTTP/1.0\n\n");
+                writer.Flush();
+
+                String data = reader.ReadLine();
+
+                while (data != null)
+                {
+                    sourceTextBox.Text += data + Environment.NewLine;
+
+                    data = reader.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
